5fe8c756df5bb02e0053a702e7b63e262fe042dd
Hyungcheol Min <jesuishc@gmail.com>
2026-09-11 10:07:52 +0900

    Use same-origin API by default

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.SNACK_CONFIG = {
-  // 6단계 백엔드를 배포한 뒤 예: https://snack-api.example.com
-  apiBase: ''
+  // 개인용 기본값: `npm start`로 실행한 현재 서버의 /api/* 를 사용합니다.
+  // 별도 백엔드를 쓰는 경우에만 전체 URL로 바꾸세요.
+  apiBase: window.location.origin
 };
